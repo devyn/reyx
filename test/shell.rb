4d0a0c8d58dc80ecfa7b620d62135dd8b97256d0
Devyn Cairns <devyn.cairns@gmail.com>
2009-02-09 12:36:24 -0800

    [test:shell] nice and pretty!

--- test/shell.rb
+++ test/shell.rb
@@ -3,12 +3,20 @@ require 'rubygems'
 require 'socket'
 require 'highline/import'
 
-sock = TCPSocket.new(ENV['REYX_HOST']||'localhost', 44698)
+def w_header(s)
+    print "\e[s\e[H\e[30;44m\e[K ::: Reyx @ #{ENV['REYX_HOST']||'localhost'} (#{s}) :::\e[0m\n\e[u"
+end
+puts "\e[2J\e[H"
+puts
+
+sock = TCPSocket.new(ENV['REYX_HOST']||'localhost', 44698) rescue abort("\e[2J\e[Herror: Can not connect to server")
+w_header 'login'
 un = ask("\e[33mUser name?\e[0m ").chomp
 pw = ask("\e[33mPassword?\e[0m  ") {|x|x.echo="*"}.chomp
 sock.puts "@ #{un}:#{pw}"
-abort("error: Invalid User Name or Password") unless sock.gets == "@ @ @   CONFIRMED   @ @ @\n"
+abort("\e[2J\e[Herror: Invalid User Name or Password") unless sock.gets == "@ @ @   CONFIRMED   @ @ @\n"
 loop do
+    w_header 'shell'
     case cmd = ask("\e[34m#{un}\e[0m $ ") {|x| x.readline=true}.chomp
     when 'exit', 'quit'
         sock.puts ">.<"
@@ -17,6 +25,7 @@ loop do
     when /^chpath /
         sock.puts "-> #{cmd.sub(/^chpath /, '')}"
     else
+        w_header cmd
         sock.puts "$ #{cmd}"
         until (ln = sock.gets) == "$ $ $   NEXT   $ $ $\n"
             if ln =~ /^\$ \$ \$   PROMPT: (.*)   \$ \$ \$$/
@@ -29,3 +38,4 @@ loop do
         end
     end
 end
+print "\e[2J\e[H"
